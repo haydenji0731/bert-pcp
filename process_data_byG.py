00b9ac6e35de1fb72ae4b1f01dc99b3d1779e15a
haydenji0731 <hji20@jhu.edu>
2023-04-29 16:25:58 -0400

    remove pretrained models, modify pre-process script to split at gene level, and add shell scripts for training and testing

--- process_data_byG.py
+++ process_data_byG.py
@@ -7,17 +7,36 @@ import pandas as pd
 
 OUTPUT_FILE = "original.tsv"
 label_d = dict()
+gtf_d = dict()
+
+
+def load_gtf(args):
+    global gtf_d
+    gtf_fh = open(args.in_gtf, 'r')
+    for line in gtf_fh:
+        if line.split("\t")[2] == "transcript":
+            infos = line.split("\t")[8].split(";")
+            for info in infos:
+                key = info.strip().split(" ")[0].strip()
+                val = info.strip().split(" ")[1].strip().replace('"', '')
+                if key == "transcript_id":
+                    tid = val
+                elif key == "gene_id":
+                    gid = val
+                    break
+        gtf_d[tid] = gid
 
 
 def kmerize(args):
     fa_fh = open(args.in_fasta, 'r')
     label_fh = open(args.label, 'r')
     global label_d
+    global gtf_d
     if not os.path.exists(args.out_dir):
         os.mkdir(args.out_dir)
     out_fn = args.out_dir + OUTPUT_FILE
     out_fh = open(out_fn, 'w')
-    out_fh.write("sequence\tlabel\n")
+    out_fh.write("gene_id\tsequence\tlabel\n")
     for line in label_fh:
         tid = line.split("\t")[0]
         label = int(line.split("\t")[1])
@@ -31,7 +50,8 @@ def kmerize(args):
                 tid = line.replace('>', '')[:-1]
             label = label_d[tid]
         else:
-            out_fh.write(seq2kmer(line[:-1], args.kmer_size) + "\t" + str(label) + "\n")
+            gid = gtf_d[tid]
+            out_fh.write(gid + "\t" + seq2kmer(line[:-1], args.kmer_size) + "\t" + str(label) + "\n")
     fa_fh.close()
     out_fh.close()
 
@@ -44,14 +64,22 @@ def split_data(args):
     neg = df_seq[df_seq['label'] == 0]
     test_pos = pos.sample(frac=args.test_frac, random_state=args.seed)
     test_neg = neg.sample(frac=args.test_frac, random_state=args.seed)
-    df_test = pd.merge(test_pos, test_neg, how='outer').sample(frac=1, random_state=args.seed)
-    pos_re = pos[~pos.sequence.isin(test_pos.sequence)].dropna()
-    neg_re = neg[~neg.sequence.isin(test_neg.sequence)].dropna()
+    test_pos_sameG = pos[pos.gene_id.isin(test_pos.gene_id)].dropna()
+    test_neg_sameG = neg[neg.gene_id.isin(test_neg.gene_id)].dropna()
+    test_pos_comb = pd.merge(test_pos, test_pos_sameG, how='outer')
+    test_neg_comb = pd.merge(test_neg, test_neg_sameG, how='outer')
+    df_test = pd.merge(test_pos_comb, test_neg_comb, how='outer').sample(frac=1, random_state=args.seed)
+    pos_re = pos[~pos.gene_id.isin(test_pos_comb.gene_id)].dropna()
+    neg_re = neg[~neg.gene_id.isin(test_neg_comb.gene_id)].dropna()
     val_pos = pos_re.sample(frac=args.val_frac, random_state=args.seed)
     val_neg = neg_re.sample(frac=args.val_frac, random_state=args.seed)
-    df_val = pd.merge(val_pos, val_neg, how='outer').sample(frac=1, random_state=args.seed)
-    train_pos = pos_re[~pos_re.sequence.isin(val_pos.sequence)].dropna()
-    train_neg = neg_re[~neg_re.sequence.isin(val_neg.sequence)].dropna()
+    val_pos_sameG = pos_re[pos_re.gene_id.isin(val_pos.gene_id)].dropna()
+    val_neg_sameG = neg_re[neg_re.gene_id.isin(val_pos.gene_id)].dropna()
+    val_pos_comb = pd.merge(val_pos, val_pos_sameG, how='outer')
+    val_neg_comb = pd.merge(val_neg, val_neg_sameG, how='outer')
+    df_val = pd.merge(val_pos_comb, val_neg_comb, how='outer').sample(frac=1, random_state=args.seed)
+    train_pos = pos_re[~pos_re.gene_id.isin(val_pos_comb.gene_id)].dropna()
+    train_neg = neg_re[~neg_re.gene_id.isin(val_neg_comb.gene_id)].dropna()
     df_train = pd.merge(train_pos, train_neg, how='outer').sample(frac=1, random_state=args.seed)
 
     test_dir = os.path.join(args.out_dir, "test")
@@ -63,30 +91,36 @@ def split_data(args):
         os.makedirs(train_val_dir)
 
     test_df_fn = os.path.join(test_dir, "dev.tsv")
+    df_test = df_test.drop('gene_id', axis=1)
     df_test.to_csv(test_df_fn, sep='\t', index=False)
     val_df_fn = os.path.join(train_val_dir, "dev.tsv")
+    df_val = df_val.drop('gene_id', axis=1)
     df_val.to_csv(val_df_fn, sep='\t', index=False)
     train_df_fn = os.path.join(train_val_dir, "train.tsv")
+    df_train = df_train.drop('gene_id', axis=1)
     df_train.to_csv(train_df_fn, sep='\t', index=False)
 
 
 def main():
     parser = argparse.ArgumentParser(description="kmer-ize input FASTA for training/testing")
     parser.add_argument("-in_fasta", type=str, help="full path please", required=True)
+    parser.add_argument("-in_gtf", type=str, help="", required=True)
     parser.add_argument("-label", type=str, help="", required=True)
     parser.add_argument("-out_dir", type=str, help="", required=True)
     # consider changing this to 6; DNABERT used 6 instead of 3
     parser.add_argument("--kmer_size", type=int, help="", default=3, required=False)
     parser.add_argument("--seed", type=int, help="", default=0, required=False)
-    parser.add_argument("--train_frac", type=float, help="", default=0.8, required=False)
-    parser.add_argument("--val_frac", type=float, help="", default=0.1, required=False)
-    parser.add_argument("--test_frac", type=float, help="", default=0.1, required=False)
+    parser.add_argument("--train_frac", type=float, help="", default=0.999, required=False)
+    parser.add_argument("--val_frac", type=float, help="", default=0.0005, required=False)
+    parser.add_argument("--test_frac", type=float, help="", default=0.0005, required=False)
     args = parser.parse_args()
     train_frac = args.train_frac
     val_frac = args.val_frac
     test_frac = args.test_frac
-    if train_frac + val_frac + test_frac != 1.0:
+    if round(train_frac + val_frac + test_frac) != 1.0:
         sys.exit("")
+    print(strftime("%Y-%m-%d %H:%M:%S | ") + "Loading annotation file.")
+    load_gtf(args)
     print(strftime("%Y-%m-%d %H:%M:%S | ") + "Starting kmer-izing sequences")
     kmerize(args)
     print(strftime("%Y-%m-%d %H:%M:%S | ") + "Kmer-ization completed. Extracting train/val/test sets.")
---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_PATH=$1
+MODEL_PATH="./ft/kmer$KMER"
+DATA_PATH="./data/test/"
+PRED_PATH="./results/kmer$KMER"
+
+# TODO: consider changing kmer size to 6
+KMER=3
+BSIZE=128
+EPOCH=3
+
+if [ -d $PRED_PATH ]
+then
+  mkdir $PRED_PATH
+fi
+
+# TODO: consider ensemble prediction
+# TODO: check max_seq_length parameter
+# TODO: check overwrite_output parameter
+python $SCRIPT_PATH --model_type dna --tokenizer_name=dna$KMER \
+  --model_name_or_path $MODEL_PATH --task_name dnaprom --do_predict \
+  --data_dir $DATA_PATH --max_seq_length 100 --per_gpu_pred_batch_size=$BSIZE \
+  --output_dir $MODEL_PATH --predict_dir $PRED_PATH --n_process 32
