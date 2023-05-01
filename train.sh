b28f8bb85d6d57b521e770758feca95675cd144d
haydenji0731 <hji20@jhu.edu>
2023-05-01 10:41:12 -0400

    update train parameters

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 # TODO: consider changing kmer size to 6
 KMER=3
 BSIZE=128
-EPOCH=1
+EPOCH=5
 
 SCRIPT_PATH=$1
 MODEL_PATH=$2
@@ -22,9 +22,9 @@ fi
 # TODO: check bsize
 python $SCRIPT_PATH --model_type dna --tokenizer_name dna$KMER \
   --model_name_or_path $MODEL_PATH --task_name dnaprom \
-  --do_train --do_eval --data_dir $DATA_PATH --max_seq_length 100 \
+  --do_train --data_dir $DATA_PATH --max_seq_length 100 \
   --per_gpu_eval_batch_size=$BSIZE --per_gpu_train_batch_size=$BSIZE \
-  --learning_rate 2e-4 --num_train_epochs $EPOCH --output_dir $OUTPUT_PATH \
-  --evaluate_during_training --logging_steps 100 --save_steps 4000 \
-  --warmup_percent 0.1 --hidden_dropout_prob 0.1 --overwrite_output \
+  --learning_rate 1e-5 --num_train_epochs $EPOCH --output_dir $OUTPUT_PATH \
+   --logging_steps 100 --save_steps 4000 \
+  --warmup_percent 0.1 --hidden_dropout_prob 0.0 --overwrite_output \
   --weight_decay 0.01 --n_process 4
