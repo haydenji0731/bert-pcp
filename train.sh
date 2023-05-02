db429e3eb5791b5169d2c8d568e69018911a8854
haydenji0731 <hji20@jhu.edu>
2023-05-02 16:54:34 -0400

    fix mistake

--- train.sh
+++ train.sh
@@ -22,7 +22,7 @@ fi
 # TODO: check bsize
 python $SCRIPT_PATH --model_type dnalong --tokenizer_name dna$KMER \
   --model_name_or_path $MODEL_PATH --task_name dnaprom \
-  --do_train --data_dir $DATA_PATH --max_seq_length 109410 \
+  --do_train --data_dir $DATA_PATH --max_seq_length 109568 \
   --per_gpu_eval_batch_size=$BSIZE --per_gpu_train_batch_size=$BSIZE \
   --learning_rate 1e-5 --num_train_epochs $EPOCH --output_dir $OUTPUT_PATH \
    --logging_steps 100 --save_steps 4000 \
